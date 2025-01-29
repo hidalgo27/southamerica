d9f4fe5f498d5f8cdaa3087035720577ca0cca6f
Julio <julio@goto.group>
2025-01-29 09:25:12 -0500

    refactor: Convert a section to a component Newsletter

--- pages/destinations.vue
+++ pages/destinations.vue
@@ -2,6 +2,7 @@
 import CardDestination from '@/components/destinations/CardDestination.vue';
 import WorldMap from '@/components/destinations/WorldMap.vue';
 import { useRoute } from 'vue-router';
+import Newsletter from '~/components/home/Newsletter.vue';
 
 const route = useRoute();
 const breadcrumbs = route.path
@@ -221,6 +222,39 @@ const continentes = [
     </div>
   </section>
   <CardDestination :destinations="continentes"></CardDestination>
+
+  <section class="container mt-28 mb-12">
+    <div class=" flex items-center justify-center ">
+      <div class="flex flex-col md:flex-row bg-white overflow-hidden w-full gap-6">
+        <div class="bg-secondary bg-opacity-10 py-32 px-16 rounded-md">
+          <p class="mb-8 tracking-widest font-bold">
+            Destination Specialists
+          </p>
+          <h1 class="font-semibold text-4xl mb-8 title font-playfair-display tracking-wide  ">
+            The experts in tailor-made travel.
+          </h1>
+          <p class="mb-8">
+            When working with Goway, you dream it, we plan it. Rely on the passion and expertise of our Destination
+            Specialists to craft the trip that’s right for you.
+          </p>
+          <div class="flex flex-col sm:flex-row sm:space-x-4 space-y-4 sm:space-y-0">
+            <button class="px-4 py-2 border btn-primary-outline rounded-md">
+              Request a Quote
+            </button>
+            <button class="px-4 py-2 border btn-secondary-outline rounded-md">
+              Meet the Team
+            </button>
+          </div>
+        </div>
+        <div class="relative w-full rounded-md">
+          <img alt="A beautiful sunset with a giraffe in the foreground" class="w-full h-full object-cover rounded-md"
+            src="https://picsum.photos/400/300" />
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <Newsletter></Newsletter>
 
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,7 @@ import TopPlaces from '~/components/home/TopPlaces.vue'
 import CardSlider from '~/components/page/slider/CardSlider.vue'
 import TravelStories from '~/components/home/TravelStories.vue'
 import Reviews from '~/components/home/Reviews.vue'
+import Newsletter from '~/components/home/Newsletter.vue'
 
 const loading = ref(true)
 const video = ref()
@@ -121,52 +122,7 @@ onMounted(async () => {
 
   <TravelStories></TravelStories>
 
-  <section class="flex items-center justify-center mt-28 mb-12 container">
-    <div class="bg-tertiary bg-opacity-10 p-10 md:p-20  rounded-md w-full">
-      <div class="flex flex-col md:flex-row items-center">
-        <div class="flex-1">
-          <p class="mb-6 tracking-widest font-bold">Newsletter</p>
-          <h1 class="font-semibold text-2xl md:text-5xl mb-6 title font-playfair-display tracking-wide">Unlock Even More
-          </h1>
-          <p class="mb-20">Sign up to our newsletter to unlock travel Specials, Inspiration, and Expert
-            Guides right to your inbox.</p>
-          <button class="btn-ternary py-2 px-4 rounded shadow-md hover:bg-gray-100 hover:text-gray-700">Sign Up
-            Today</button>
-        </div>
-        <div class="hidden md:block border-l border-gray-200 h-96 mx-8"></div>
-        <div class="block md:hidden border-b border-gray-200 w-full my-8"></div>
-        <div class="flex-1">
-          <ul class="space-y-8">
-            <li class="flex items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="mr-2 w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6Z" />
-              </svg>
-              Exclusive Savings
-            </li>
-            <li class="flex items-center ">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="mr-2 w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M21 11.25v8.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 1 0 9.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1 1 14.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
-              </svg>
-              Insider Contests
-            </li>
-            <li class="flex items-center ">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="mr-2 w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
-              </svg>
-              Trending Guides
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </section>
+  <Newsletter></Newsletter>
 
   <Reviews></Reviews>
 
