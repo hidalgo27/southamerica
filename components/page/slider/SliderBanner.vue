9fd6386346f1fe1d6ab07a0356887f9f5bd5ff95
Julio <julio@goto.group>
2025-01-30 09:35:40 -0500

    refactor: Change the name of SliderDestination to SliderBanner, correct the padding between banners and delete unnecessary component

--- components/page/slider/SliderBanner.vue
+++ components/page/slider/SliderBanner.vue
@@ -16,20 +16,18 @@ const slides = [
 <template>
   <section class="my-20">
     <carousel :items-to-show="1.2" :gap="200">
-      <slide v-for="(slide, index) in slides" :key="index">
-        <img :src="slide.image" alt="Slide Image" class="w-full h-[60vh] object-cover p-12 rounded-md" />
+      <slide v-for="(slide, index) in slides" :key="index" class="px-3">
+        <img :src="slide.image" alt="Slide Image" class="w-full h-[60vh] object-cover  rounded-md" />
         <div class="absolute inset-0 flex flex-col items-start justify-center p-8 md:p-16 lg:p-24">
           <h1 class="text-white text-4xl md:text-5xl lg:text-6xl font-bold font-playfair">
             {{ slide.title }}
           </h1>
-          <button class="mt-4 px-6 py-3 bg-white text-gray-800 text-lg font-semibold rounded shadow hover:bg-gray-100">
+          <button
+            class="translate-y-44 px-6 py-3 bg-white text-gray-800 text-lg font-semibold rounded shadow hover:bg-gray-100">
             {{ slide.buttonText }}
           </button>
         </div>
       </slide>
-      <template #addons>
-        <CarouselPagination />
-      </template>
     </Carousel>
   </section>
 </template>
