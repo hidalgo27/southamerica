bc6614274e41fbc3704a93f8a828949d66565180
Julio <julio@goto.group>
2025-02-03 12:07:42 -0500

    feat: Add library amCharts to create maps and add map of South America

--- components/destinations/WorldMap.vue
+++ components/destinations/WorldMap.vue
@@ -1,13 +1,127 @@
-<template>
-  <div>
-    
-  </div>
-</template>
+<script setup lang="ts">
+import * as am5 from "@amcharts/amcharts5";
+import * as am5map from "@amcharts/amcharts5/map";
+import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
+import am5geodata_southAmericaLow from "@amcharts/amcharts5-geodata/region/world/southAmericaLow";
+import { useRouter } from 'vue-router';
 
-<script lang="ts" setup>
+const chartDiv = ref<HTMLElement | null>(null);
+const router = useRouter();
 
+onMounted(() => {
+  if (chartDiv.value) {
+    const root = am5.Root.new(chartDiv.value);
+    root.setThemes([am5themes_Animated.new(root)]);
+
+    const chart = root.container.children.push(
+      am5map.MapChart.new(root, {
+        panX: "rotateX",
+        projection: am5map.geoMercator(),
+        layout: root.horizontalLayout
+      })
+    );
+
+    const polygonSeries = chart.series.push(
+      am5map.MapPolygonSeries.new(root, {
+        geoJSON: am5geodata_southAmericaLow,
+      })
+    );
+
+    polygonSeries.mapPolygons.template.setAll({
+      tooltipHTML: `<div class="font-bold tracking-wide"> {name} </div>`,
+      interactive: true,
+      cursorOverStyle: "pointer",
+    });
+
+    polygonSeries.mapPolygons.template.events.on("click", (ev) => {
+      const countryName = ev.target.dataItem?.dataContext.name;
+      if (countryName) {
+        router.push(`/destinations/${countryName.toLowerCase()}`);
+      }
+    });
+
+    polygonSeries.mapPolygons.template.states.create("hover", {
+      fill: am5.color(0x677935),
+    });
+
+    const buttons = chart.children.push(
+      am5.Container.new(root, {
+        x: am5.p100, // Mueve los botones a la derecha
+        centerX: am5.p100,
+        y: am5.p50,
+        centerY: am5.p50,
+        layout: root.verticalLayout, // Ajusta la disposición vertical
+        paddingTop: 5,
+        paddingRight: 8,
+        paddingBottom: 5,
+        paddingLeft: 8,
+        background: am5.RoundedRectangle.new(root, {
+          fill: am5.color(0xffffff),
+          fillOpacity: 0.3,
+        }),
+        visible: window.innerWidth >= 768,
+      })
+    );
+
+    // Función para crear los botones de países
+    function createCountryButton(countryName: string) {
+      const button = buttons.children.push(
+        am5.Button.new(root, {
+          paddingTop: 5,
+          paddingRight: 10,
+          paddingBottom: 5,
+          paddingLeft: 10,
+          maxWidth: 150,
+          centerX: am5.p50,
+          width: 130,
+          cursorOverStyle: "pointer",
+          label: am5.Label.new(root, {
+            text: countryName,
+            fontSize: 14,
+            textAlign: "center",
+
+          }),
+        })
+      );
+
+      // Hover: Resalta el país en el mapa
+      button.events.on("pointerover", () => {
+        polygonSeries.mapPolygons.each((polygon) => {
+          if (polygon.dataItem?.dataContext.name === countryName) {
+            polygon.set("fill", am5.color(0x677935)); // Resalta el país en amarillo
+          }
+        });
+      });
+
+      // Hover Out: Quita el resaltado
+      button.events.on("pointerout", () => {
+        polygonSeries.mapPolygons.each((polygon) => {
+          if (polygon.dataItem?.dataContext.name === countryName) {
+            polygon.set("fill", am5.color(0x6794DC));
+          }
+        });
+      });
+
+      // Click: Redirige a la página del país
+      button.events.on("click", () => {
+        router.push(`/destinations/${countryName.toLowerCase()}`);
+      });
+    }
+    const countries = ["Brazil", "Argentina", "Peru", "Chile", "Colombia", "Ecuador", "Bolivia", "Paraguay", "Uruguay", "Venezuela", "Guyana", "Suriname", "French Guiana", "Falkland Islands"];
+    countries.forEach(createCountryButton);
+  }
+});
 </script>
 
-<style>
+<template>
+  <section class="container my-20">
+    <div ref="chartDiv" class="map-container rounded-md"></div>
+  </section>
+</template>
 
+<style scoped>
+.map-container {
+  width: 100%;
+  @apply h-[65vh]
+}
 </style>
--- pages/destinations/index.vue
+++ pages/destinations/index.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
 import CardDestination from '@/components/destinations/CardDestination.vue';
 import WorldMap from '@/components/destinations/WorldMap.vue';
+import DescriptionWithLine from '~/components/home/DescriptionWithLine.vue';
 import EspecialistLetter from '~/components/home/EspecialistLetter.vue';
 import MiniReviews from '~/components/home/MiniReviews.vue';
 import Newsletter from '~/components/home/Newsletter.vue';
+import TextDescription from '~/components/home/TextDescription.vue';
 import HeaderImgNav from '~/components/page/HeaderImgNav.vue';
 
 import { useDestinationStore } from '~/stores/destination';
@@ -14,20 +16,10 @@ const destinationStore = useDestinationStore();
 </script>
 <template>
   <HeaderImgNav></HeaderImgNav>
-  <section class="container mt-28 mb-12">
-    <div class="text-center ">
-      <div class="border-title mb-2 mx-auto"></div>
-      <h1 class="font-semibold text-5xl mb-6 title font-playfair-display tracking-wide"> Where Are You Headed To Next?
-      </h1>
-      <div class="w-1/2 mx-auto my-8">
-        <p class="tracking-widest font-light">Experience all that the world has to offer in just the way you want. Our
-          team of Destination Specialists have been tailoring dream vacations for over 50 years. We handle every part of
-          the travel journey, from flights to tours to accommodations to group travel. Let us do the hard work so you
-          can sit back, relax, and take in the world. Rely on personalized travel planning and enjoy elevated
-          experiences in destination. Follow your fancy and explore new parts of the world in new ways.</p>
-      </div>
-    </div>
-  </section>
+  <DescriptionWithLine></DescriptionWithLine>
+
+  <WorldMap></WorldMap>
+
   <CardDestination :destinations="destinations"></CardDestination>
 
   <EspecialistLetter></EspecialistLetter>
