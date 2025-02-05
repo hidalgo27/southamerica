9cc9082175ef7e392a4e5b0817a871c4aae19cd1
Julio <julio@goto.group>
2025-02-05 17:11:17 -0500

    feat: Add our-policies page

--- pages/about-us/careers.vue
+++ pages/about-us/careers.vue
@@ -144,7 +144,6 @@ const positions = [
               stroke="currentColor" class="size-4 ml-2">
               <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
             </svg>
-
           </NuxtLink>
         </div>
       </div>
