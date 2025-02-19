aa8f60f14b9a206c8e9bab9a58e0a571a763777f
Julio <julio@goto.group>
2025-02-19 16:39:36 -0500

    feat: Add the travel guides section to the travel guides pages

--- pages/destinations/[country]/[region]/travel-guides.vue
+++ pages/destinations/[country]/[region]/travel-guides.vue
@@ -5,11 +5,13 @@ import EspecialistLetter from '~/components/home/EspecialistLetter.vue';
 import MiniReviews from '~/components/home/MiniReviews.vue';
 import Newsletter from '~/components/home/Newsletter.vue';
 import HeaderImgNav from '~/components/page/HeaderImgNav.vue';
+import TravelGuides from '~/components/destinations/TravelGuides.vue';
 </script>
 <template>
   <HeaderImgNav></HeaderImgNav>
   <NavDestination></NavDestination>
   <CardBlue></CardBlue>
+  <TravelGuides></TravelGuides>
   <EspecialistLetter> </EspecialistLetter>
   <Newsletter></Newsletter>
   <MiniReviews></MiniReviews>
