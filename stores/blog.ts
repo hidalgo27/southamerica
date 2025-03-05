de5aec734a68d5a84043796ddf608fd98df88181
Julio <julio@goto.group>
2025-03-05 17:15:52 -0500

    feat: Add the function to get the detail of one blog

--- stores/blog.ts
+++ stores/blog.ts
@@ -24,7 +24,30 @@ export const useBlogStore = defineStore('blog', () => {
       }
     })
   }
+
+  const getBlog = async (name: string) => {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    return new Promise(async (resolve, reject) => {
+      try {
+        const res = await fetch(config.public.apiBase + "/blog/" + name, {
+          method: 'GET',
+          headers: headers,
+        })
+        const data = await res.json()
+        if (data) {
+          resolve(data)
+        } else {
+          reject(data)
+        }
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
   return {
     getBlogs,
+    getBlog,
   }
 }, { persist: true, })
